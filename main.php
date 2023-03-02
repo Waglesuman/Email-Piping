fcc17a33cbdca16506108e297e20f4c384c7140a
Suman <sumanwagle7000@gmail.com>
2023-03-02 17:19:03 +0545

    double post

--- main.php
+++ main.php
@@ -1,16 +1,16 @@
 <?php
 /*
-  Plugin Name: Email Piping Plugin 
-  Plugin URI: http://example.com/
-  Description: Create posts via email.
-  Version: 1.0.0
-  Author: Suman Wagle
-  Author URI: http://emailpiping.com/
-  License: GPL3
-  Text Domain: cpmemailpiping
- */
+Plugin Name: Email Piping Plugin 
+Plugin URI: http://example.com/
+Description: Create posts via email.
+Version: 1.0.0
+Author: Suman Wagle
+Author URI: http://emailpiping.com/
+License: GPL3
+Text Domain: cpmemailpiping
+*/
 
- // If this file is called directly, abort.
+// If this file is called directly, abort.
 if (!defined('WPINC')) {
   die;
 }
@@ -22,53 +22,62 @@ if (!defined('CPM_PLUGIN_DIR')) {
 if (!defined('CPM_PLUGIN_DIR_PATH')) {
   define('CPM_PLUGIN_DIR_PATH', plugin_dir_path(__FILE__));
 }
-// Server details
-$email = 'test.sumanwagle@gmail.com';
-$password = 'eixbjprgzpzvzxow';
-$server = '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX';
 
-// Connect to the server
-$imap = imap_open($server, $email, $password) or die('Cannot connect to the server: ' . imap_last_error());
+add_action('init', 'email_piping_init');
 
-// Get the number of messages in the inbox
-$num_msgs = imap_num_msg($imap);
+function email_piping_init()
+{
+  if (extension_loaded('imap')) {
+    // Server details
+    $email = 'test.sumanwagle@gmail.com';
+    $password = 'eixbjprgzpzvzxow';
+    $server = '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX';
 
-// Loop through each message and create a new WordPress post
-for ($i = 1; $i <= $num_msgs; $i++) {
-  
-  // Read the email message contents
-  $email_msg = imap_fetchbody($imap, $i, "1.2");
-  if ($email_msg == "") {
-    $email_msg = imap_fetchbody($imap, $i, "1");
-  }
-  $email_msg = quoted_printable_decode($email_msg);
+    // Connect to the server
+    $imap = imap_open($server, $email, $password) or die('Cannot connect to the server: ' . imap_last_error());
 
-  // Extract the email address and subject
-  $message = imap_fetchheader($imap, $i);
-  $email_address = imap_rfc822_parse_headers($message)->from[0]->mailbox . "@" . imap_rfc822_parse_headers($message)->from[0]->host;
-  $subject = imap_utf8(imap_fetchheader($imap, $i, FT_PREFETCHTEXT));
+    // Get the number of messages in the inbox
+    $MC = imap_check($imap);
+    $num = $MC->Nmsgs;
 
-  // Create a new WordPress post
-  require_once(ABSPATH . 'wp-load.php');
-  $post_title = $subject; // Set the post title
-  $post_content = $email_msg; // Use the email message as the post content
+    // Fetch the latest message in the mailbox
+    $result = imap_fetch_overview($imap, "$num", 0);
 
-  // Create the new post
-  $new_post = array(
-    'post_title' => $post_title,
-    'post_content' => $post_content,
-    'post_status' => 'publish'
-  );
-  $post_id = wp_insert_post($new_post);
+    // Check if there is a message to process
+    if (count($result) > 0) {
+      // Get the message details
+      $overview = array_shift($result);
+      $msgno = $overview->msgno;
+      $subject = $overview->subject;
+      $body = imap_fetchbody($imap, $msgno, 1);
 
-  // Redirect the user to the new post
-  if ($post_id) {
-    wp_redirect(get_permalink($post_id));
-    exit;
-  } else {
-    echo 'Error creating post';
+      // Check if the message has not been processed before
+      $processed_messages = get_option('processed_messages', array());
+      if (!in_array($msgno, $processed_messages)) {
+        // Check if the message has a subject and body
+        if (!empty($subject) && !empty($body)) {
+          // Create a new WordPress post
+          $post_title = $subject; // Set the post title
+          $post_content = $body; // Use the email message as the post content
+          // Create the new post
+          $new_post = array(
+            'post_title' => $post_title,
+            'post_content' => $post_content,
+            'post_status' => 'publish'
+          );
+          wp_insert_post($new_post);
+
+          // Update the list of processed messages
+          $processed_messages[] = $msgno;
+          update_option('processed_messages', $processed_messages);
+        } else {
+          // Log an error if the message is empty
+          error_log('Empty subject or body in email message');
+        }
+      }
+    }
+
+    // Close the IMAP connection
+    imap_close($imap);
   }
 }
-
-// Close the IMAP connection
-imap_close($imap);
